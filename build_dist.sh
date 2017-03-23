2d89d511f14cc1dbd23c3ebdb76190920fe3aa48
LimeXPS <deathpoison.dc@gmail.com>
2017-03-23 23:42:06 +0100

    added dist dir to gitignore modified build script to keep old builds

--- build_dist.sh
+++ build_dist.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-rm -rf dist_*
 rm -rf mPad-*
 yarn run build
 tar czf mPad-darwin-x64.tar.gz mPad-darwin-x64/
@@ -10,7 +9,6 @@ tar czf mPad-linux-x64.tar.gz mPad-linux-x64/
 tar czf mPad-mas-x64.tar.gz mPad-mas-x64/
 tar czf mPad-win32-ia32.tar.gz mPad-win32-ia32/
 tar czf mPad-win32-x64.tar.gz mPad-win32-x64/
-DISTNAME=dist_`date +%Y-%m-%d`
-mkdir $DISTNAME
-mv mPad*tar.gz $DISTNAME
-rm -rf mPad-*
+DISTNAME=`date +%Y-%m-%d`_`date +%s` #_`php -r "echo uniqid();"`
+mkdir -p dist/$DISTNAME
+mv mPad*tar.gz dist/$DISTNAME
